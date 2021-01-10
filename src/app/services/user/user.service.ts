6b4cca498ff5692a032418f0af4f978f30e9640e
Ilya Sukhorukov <absolutely1756@gmail.com>
2021-01-10 20:39:55 +0700

    1. Если лог пустой - выводим соответствующее сообщение 2. Изменил адрес на получение всех учебных групп

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -24,7 +24,7 @@ export class UserService {
   }
 
   getGroups(): Observable<any> {
-    return this.http.get<any>(this.appConfig.webServiceApi('/dict/group'));
+    return this.http.get<any>(this.appConfig.webServiceApi('/studyGroup'));
   }
 
   createCalendarEvent(request: any): Observable<any> {
