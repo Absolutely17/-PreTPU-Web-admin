5cb63bdc66450927d8f875598ac6903b587349b9
Ilya Sukhorukov <absolutely1756@gmail.com>
2021-03-13 17:59:34 +0700

    1. Добавили стили для скроллбара

--- src/app/components/common/ckeditor/ckeditor-constants.ts
+++ src/app/components/common/ckeditor/ckeditor-constants.ts
@@ -4,7 +4,21 @@ export const topTags = '<!DOCTYPE HTML>\n' +
   '  <meta charset=\\"utf-8\\">\n' +
   '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
   '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
-  '<style>img {height: auto !important} div {overflow-wrap: break-word}' +
+  '<style>::-webkit-scrollbar {\n' +
+  '    width: 12px;\n' +
+  '}\n' +
+  ' \n' +
+  '::-webkit-scrollbar-track {\n' +
+  '    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); \n' +
+  '    border-radius: 10px;\n' +
+  '}\n' +
+  ' \n' +
+  '::-webkit-scrollbar-thumb {\n' +
+  '    border-radius: 10px;\n' +
+  '    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); \n' +
+  '} ' +
+  'img {height: auto !important} ' +
+  'div {overflow-wrap: break-word}' +
   '</style></head>\n' +
   '<body>';
 
